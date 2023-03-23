df6364d8db1438aed10ec63a88dbda3297d93022
Tan <>
2023-03-23 09:40:52 +0700

    setup components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Footer, Navbar, Services, Transactions, Welcome } from './components';
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
