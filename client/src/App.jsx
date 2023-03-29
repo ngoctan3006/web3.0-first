3370dfd2382586da30d978d68dd28102041bd8da
Tan <>
2023-03-29 17:00:14 +0700

    setup tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const App = () => {
-  return <h1 className="text-3xl font-bold">Hello</h1>;
+  return (
+    <div className="relative sm:p-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">Sidebar</div>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        Navbar
+      </div>
+    </div>
+  );
 };
 
 export default App;
