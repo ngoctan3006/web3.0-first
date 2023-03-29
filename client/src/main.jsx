288639ced17f92124884525e5b5043bb43304169
Tan <>
2023-03-29 15:55:51 +0700

    setup fe

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import { ThirdwebProvider } from '@thirdweb-dev/react';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+const activeChain = 'goerli';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <ThirdwebProvider activeChain={activeChain}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThirdwebProvider>
+  </React.StrictMode>,
+);
